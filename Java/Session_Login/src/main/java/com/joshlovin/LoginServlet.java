04929a32d57ee53c02b6145588feed4589147fe7
jdlovin <josh.lovin@revature.net>
2021-08-23 16:50:21 -0400

    Old commits

--- Java/Session_Login/src/main/java/com/joshlovin/LoginServlet.java
+++ Java/Session_Login/src/main/java/com/joshlovin/LoginServlet.java
@@ -1,5 +1,25 @@
 package com.joshlovin;
 
-public class LoginServlet {
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+public class LoginServlet extends HttpServlet {
+	
+	public void doPost (HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		
+		request.getRequestDispatcher("navbar.html").include(request, response);
+		
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+		
+		
+	}
 
 }
