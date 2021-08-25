0c261ef0ba1ed0b2c486d8cd7862a8da86f63dcf
jdlovin <josh.lovin@revature.net>
2021-08-25 15:27:04 -0400

    trying out a private variable

--- Week6/Day3/demo16.js
+++ Week6/Day3/demo16.js
@@ -10,8 +10,18 @@ var Student = /** @class */ (function () {
             throw new Error('Id can not be negative');
         this.id = value;
     };
+    Student.prototype.setX = function (value) {
+        this.x = value;
+    };
+    Student.prototype.getX = function () {
+        return this.x;
+    };
     return Student;
 }());
 var student = new Student();
 student.setId(25);
+student.setX('Bob');
+console.log(student.getX());
+student.setX(23);
+console.log(student.getX());
 console.log(student.getId());
--- Week6/Day3/demo16.ts
+++ Week6/Day3/demo16.ts
@@ -2,6 +2,7 @@ class Student{
     private id: number;
     private name: string;
     private passMark = 40;
+    private x;
 
     getId(){
         return this.id;
@@ -12,9 +13,22 @@ class Student{
             throw new Error('Id can not be negative')
         this.id = value;
     }
+
+    setX(value){
+        this.x = value
+    }
+
+    getX(){
+        return this.x;
+    }
 }
 
 let student = new Student();
 student.setId(25);
+
+student.setX('Bob');
+console.log(student.getX());
+student.setX(23);
+console.log(student.getX());
 
 console.log(student.getId());
